6736723b7ab26061900c50e9b685787e50d24c8f
josejesusgarciajr <josejesusgarciajr@gmail.com>
2019-03-22 13:24:05 -0700

    Ships almost implemented, Brandon will finish ship implementation

--- Battle/Player.py
+++ Battle/Player.py
@@ -4,15 +4,27 @@ Created on Mar 22, 2019
 @author: jginvincible
 '''
 
+from Ship import Ship
+
 class Player:
     def __init__(self, name, status, wins):
         self.name = name
         self. status = status
         self.wins = wins
         self.grid = self.makeMap()
+        self.ships = [Ship("Aircraft Carrier", 5),
+                      Ship("Battleship", 4),
+                      Ship("Submarine", 3),
+                      Ship("Cruiser", 3),
+                      Ship("Destroyer", 2)]
+        self.placeShips()
     
     def makeMap(self):  ## generates a 9 by 8 matrix as the grid
         return [['#' for x in range(9)] for y in range(8)]
+    
+    def placeShips(self):
+        for ship in self.ships:
+            print("Implement this methon Brandon A.")
     
     def validCoordinates(self, x , y):      # checks whether or not coordinates are valid
         print("X: " + str(x))
---
+++ Battle/Ship.py
@@ -0,0 +1,12 @@
+'''
+Created on Mar 22, 2019
+
+@author: jginvincible
+'''
+
+class Ship:
+    def __init__(self, name, length):
+        self.name = name
+        self.length = length
+        self.x = None
+        self.y = None
